ac293e4a1cad5bf464522b0ef5fd48ee2bf0402b
Steffen Gebert <steffen.gebert@typo3.org>
2016-03-19 21:39:44 +0100

    Overwork metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,20 @@
-name             'site-skeletontypo3org'
-maintainer       'The TYPO3 DevOps Team'
-maintainer_email 'cookbooks@typo3.de'
-license          'All rights reserved'
-description      'Skeleton Cookbook for TYPO3 Site Cookbooks'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+name             "site-skeletontypo3org"
+maintainer       "TYPO3 Server Admin Team"
+maintainer_email "cookbooks@typo3.org"
+license          "Apache 2.0"
+description      "Skeleton Cookbook for TYPO3 Site Cookbooks"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "1.0.0"
 
-supports         'debian'
+supports         "debian"
+
+# base cookbook
+depends          "t3-base",     "~> 0.2.0"
+
+# TYPO3-cookbooks, pin to minor (~> 1.1.0)
+# depends          "zabbix-custom-checks", "~> 0.2.0"
+# depends          "ssl_certificates",     "~> 0.2.0"
+
+# community cookbooks, pin to patchlevel (= 1.1.1)
+# depends          "haproxy",    "= 1.6.7"
+# depends          "openssl",    "= 4.4.0"
