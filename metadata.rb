514234e129e98a9487821b2be9fcbdff9356bea9
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-14 13:46:00 +0200

    [TASK] Switch to v2 pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@typo3.org"
 license          "Apache 2.0"
 description      "Wrapper cookbook for Docker"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.1"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports         "debian"
 
