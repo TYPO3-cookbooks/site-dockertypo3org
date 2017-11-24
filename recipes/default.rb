5f5d6d42a95489a40447379b312e045aa6b47cd1
Andri Steiner <andri@snowflakeops.ch>
2017-11-24 19:23:39 +0100

    [TASK] #80770 use systemd_service instead of template resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,9 +23,30 @@ end
 package 'docker-engine'
 
 # Service configuration
-template '/lib/systemd/system/docker.service' do
-  source "docker.service"
-  mode 0644
+systemd_service 'docker' do
+  description 'Docker Application Container Engine'
+  documentation 'https://docs.docker.com'
+  after 'network-online.target docker.socket firewalld.service'
+  wants 'network-online.target'
+  requires 'docker.socket'
+  install do
+    wanted_by 'multi-user.target'
+  end
+  service do
+    type 'notify'
+    exec_start '/usr/bin/dockerd --tlsverify=true --tlscacert=/etc/docker/tls.crt --tlscert=/etc/docker/tls.crt --tlskey=/etc/docker/tls.key -H=0.0.0.0:2376 --host=unix:///var/run/docker.sock'
+    exec_reload '/bin/kill -s HUP $MAINPID'
+    limit_nofile 1048576
+    limit_nproc 'infinity'
+    limit_core 'infinity'
+    timeout_start_sec 0
+    delegate true
+    kill_mode 'process'
+    restart 'on-failure'
+    start_limit_burst 3
+    start_limit_interval '60s'
+  end
+  only_if { ::File.open('/proc/1/comm').gets.chomp == 'systemd' } # systemd
 end
 
 # Service
