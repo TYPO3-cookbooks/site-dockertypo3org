693dd6beb8c77697b1c5c7fa44604d113f198ced
Michael Stucki <michael@stucki.io>
2019-11-05 18:48:17 +0100

    [TASK] Update docker-compose to 1.24.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@
 default['email_adress'] = 'cookbooks@typo3.org'
 
 #<> docker-compose version (from GitHub release)
-default['docker-compose']['version'] = '1.24.0'
+default['docker-compose']['version'] = '1.24.1'
